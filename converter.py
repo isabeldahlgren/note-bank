f67e4eff3fa50efa45d98a5357f991421faf5476
Isabel <isabel.dahlgren@gmail.com>
2022-07-28 17:29:18 +0200

    with support for enumerate and itemize

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 
 def converter(latex_files):
@@ -9,19 +10,18 @@ def converter(latex_files):
             txt = file.read()
 
             # TODO: Add a for loop
-            # TODO: Add support for enumerate
 
             # Trimming document
             txt = txt[(txt.find('\\begin{document}') + 16):txt.find('\\end{document}')]
 
             # Creating titles
             txt = re.sub('\\\\section(\*)?{(.*)}', r'<h3 id="\2"><a class="hashtag" href="#\2">#</a>\2</h3><p>', txt)
-            txt = re.sub('\\\\subsection(\*)?{(.*)}', r'<h4 id="\2"><a class="hashtag" href="#\2">#</a>\2</h4><p>', txt)
+            txt = re.sub('\\\\subsection(\*)?{(.*)}', r'</p><h4 id="\2"><a class="hashtag" href="#\2">#</a>\2</h4><p>', txt)
 
             # Creating theorem/definition cards
-            txt = re.sub('(\\\\begin{theorem})|(\\\\begin{definition})', '</p><div class="card"><div class="card-body">', txt)
-            txt = re.sub('(\\\\end{theorem})|(\\\\end{definition})', '</div></div><p>', txt)
-            txt = re.sub('\[(.*)\]', r'<b>\1</b><br>', txt)
+            txt = re.sub('\\\\begin\{(theorem|definition)\}', '</p><div class="card"><div class="card-body">', txt)
+            txt = re.sub('\\\\end\{(theorem|definition)\}', '</div></div><br><p>', txt)
+            txt = re.sub('\[(.*)\]', r'<b>\1</b>', txt)
 
             # Creating block quotes
             txt = re.sub('\\\\somequote{(.*)}{(.*)}{(.*)}', r'''<figure>
@@ -33,6 +33,12 @@ def converter(latex_files):
             </figcaption>
             </figure>
             ''', txt)
+
+            txt = re.sub('\\\\begin{enumerate}', '<ol>', txt)
+            txt = re.sub('\\\\end{enumerate}', '</ol>', txt)
+            txt = re.sub('\\\\begin{itemize}', '<ul>', txt)
+            txt = re.sub('\\\\end{itemize}', '</ul>', txt)
+            txt = re.sub('\\\\item (.*)', r'<li>\1</li>', txt)
 
             # Adding to result and resetting txt
             res += f'\n\n{txt}'
